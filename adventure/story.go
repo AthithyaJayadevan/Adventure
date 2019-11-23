d2ae8e5394dc264dd3579ea22f1e80ab3520a819
Athithya Jayadevan (atjayade) <atjayade@cisco.com>
2019-11-23 15:09:57 -0800

    CHOOSE YOUR OWN ADVENTURE implemented with a simple HTML template

--- adventure/story.go
+++ adventure/story.go
@@ -3,27 +3,92 @@ package adventure
 import (
 	"encoding/json"
 	"io"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
 )
 
+// Story exported type
 type Story map[string]Storyarc
 
+//var tpl *template.Template
+
+var defaulthandlertemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose your own ADVENTURE</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Story}}
+        <p> {{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+			<li> <a href="/{{.Arc}}">{{.Text}}</a></li>
+			{{end}}
+        </ul>
+    </body>
+</html>`
+
+//func init() {
+//	tpl = template.Must(template.New("").Parse(defaulthandlertemplate))
+//
+//}
+
+type handler struct {
+	s Story
+}
+
+//Newhandler function to handle web requests
+func Newhandler(s Story) http.Handler {
+	return handler{s}
+
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.New("").Parse(defaulthandlertemplate))
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	// "/intro" -> "intro"
+	path = path[1:]
+
+	if text, ok := h.s[path]; ok {
+		err := tpl.Execute(w, text)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found...", http.StatusNotFound)
+}
+
+// Jsonparser to parse the json data to a struct type
 func Jsonparser(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
 	var story Story
-	if err := d.Decode(&story); err != nil {
-		panic(err)
+	err := d.Decode(&story)
+	if err != nil {
+		return nil, err
 	}
 	return story, nil
+
 }
 
+//Option USED TO STORE OPTION
 type Option struct {
-	//USED TO STORE OPTION
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
 }
 
+//Storyarc USED TO STORE STORY
 type Storyarc struct {
-	//USED TO STORE STORY
 	Title   string   `json:"title"`
 	Story   []string `json:"story"`
 	Options []Option `json:"options"`
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"cyoa_own/adventure"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	file := flag.String("file", "gopher.json", "The JSON file that's going to be parsed")
+	port := flag.Int("port", 3000, "The port that will use the cyoa application")
 	flag.Parse()
 	f, err := os.Open(*file)
 	if err != nil {
@@ -21,8 +24,8 @@ func main() {
 		panic(err)
 	}
 
-	//REFACTORING
-
-	fmt.Printf("%+v\n", story)
+	h := adventure.Newhandler(story)
+	fmt.Printf("Starting server at : %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 
 }
